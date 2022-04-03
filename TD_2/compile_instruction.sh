4592ac5505cb6ce1a7267ec08c0ad9d78bfae629
Rescon <yu.wang@ensta-paris.fr>
2022-04-03 21:38:26 +0200

    ROB305

--- TD_2/compile_instruction.sh
+++ TD_2/compile_instruction.sh
@@ -1,3 +1,3 @@
-arm-linux-g++ -Wall -Wextra main_td2a.cpp -o main_td2a -lpthread
-arm-linux-g++ -Wall -Wextra main_td2b.cpp timespec.cpp -o main_td2b -lrt -lpthread
-arm-linux-g++ -Wall -Wextra main_td2c.cpp timespec.cpp -o main_td2c -lrt -lpthread
+arm-linux-g++ -Wall -Wextra src/main_td2a.cpp -o main_td2a -lpthread
+arm-linux-g++ -Wall -Wextra src/main_td2b.cpp src/timespec.cpp -o main_td2b -lrt -lpthread
+arm-linux-g++ -Wall -Wextra src/main_td2c.cpp src/timespec.cpp -o main_td2c -lrt -lpthread
